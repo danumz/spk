50f2dc172f0bbcf7796f52fcc1172fff05adc03b
danumz <maulanazakaria.danu@gmail.com>
2019-11-28 10:31:09 +0700

    update dashboard.php

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -11,7 +11,7 @@
                 <h3 class="text-themecolor">Dashboard 2</h3>
                 <ol class="breadcrumb">
                     <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
-                    <li class="breadcrumb-item active">Dashboard baru</li>
+                    <li class="breadcrumb-item active">Dashboard barusansasa</li>
                 </ol>
             </div>
             <div class="col-md-7 col-4 align-self-center">
